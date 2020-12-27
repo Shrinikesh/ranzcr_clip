c0e8433937b5b9c199995af17d1189672d02a2c6
Shrinikesh Arulselvan <sa787@cam.ac.uk>
2020-12-27 19:21:12 +0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Codebase for the RANZCR CLiP - Catheter and Line Position Challenge: https://www.kaggle.com/c/ranzcr-clip-catheter-line-classification/overview/description',
+    author='Shrinikesh Arulselvan',
+    license='MIT',
+)
